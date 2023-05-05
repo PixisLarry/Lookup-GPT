527fa4243140167f205a9b34564c5e2d1fd4150e
MakotoAtsu <chris03152008@gmail.com>
2023-05-05 03:27:15 +0000

    Upgrade vite-plugin-crx-mv3 version

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,7 @@ export default defineConfig(({ mode }) => {
         build: {
             chunkSizeWarningLimit: 1000,
             emptyOutDir: prod,
-            terserOptions: {
-                keep_classnames: !prod,
-                keep_fnames: !prod,
-            },
         },
-        minify: prod,
+        minify: prod
     }
 })
